4934b0fcb7d94a4417d0c4c4e2d281622f56d86e
Andrew <andre.gutierrez9190@gmail.com>
2026-05-26 10:39:14 -0300

    DB

--- Project/Api/models.py
+++ Project/Api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Alumnos(models.Model):
+    ID_Alumno=models.AutoField(primary_key=True)
+    DNI=models.TextField(max_length=10)
+    Apellido=models.TextField(max_length=35)
+    Nombre=models.TextField(max_length=35)
+    Edad=models.ImageField()
+    Calle=models.TextField(max_length=50)
+    Altura=models.ImageField()
+
+    def __str__(self):
+        return self.DNI
+        
